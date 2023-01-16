b4745188dfd7b5983bc2f4f5dbc35441f6265b91
ddwin <86922608+ddwin@users.noreply.github.com>
2023-01-16 17:26:06 +0800

    Update rescue.sh

--- rescue.sh
+++ rescue.sh
@@ -15,11 +15,6 @@ fi
 #   ./rescue.sh -v
 #   ./rescue.sh --version
 
-log() {
-  echo "$1"
-  read -r -p "Press enter to continue"
-}
-
 version() {
   printf '\nScript version: %s\n  Release date: %s' "${VERSION}" "${RELEASE_DATE}"
 
@@ -44,12 +39,12 @@ Usage:
 ----------------------------------------------------------------------------------------------------
 Run the script as sudoer:
 
-  sudo ${0} [options] [parameters]
+  sudo ${0} [options]
 
 or if your operating system does not use sudo:
 
   su -
-  ${0} [options] [parameters]
+  ${0} [options]
 
 To get version number, release date, last git commit and git retrieval date, with:
 
@@ -61,10 +56,14 @@ To get this help text, with:
   ${0} -h
   ${0} --help
 
-The last development version of Script can be downloaded, with:
+To get last version of script, with:
 
   ${0} -u
   ${0} --update
+
+To start rescue mode, with:
+
+  ${0}
 
 HELP
 }
@@ -88,23 +87,25 @@ update() {
 
   # Get current UTC time in YYYY-MM-DD_hh:mm:ss format.
   UTC_TIME=$(date --utc "+%Y-%m-%d_%T")
-  log "${UTC_TIME}"
   FILENAME="${HOME}/rescue_${UTC_TIME}.sh"
-  log "${FILENAME}"
 
   # Check if wget or curl is available
   if [[ $(type wget echo $? >/dev/null 2>&1) -eq 0 ]]; then
-    printf '\nDownloading last development version of Script from git:\n\n'
-    LAST_GIT_COMMIT_ID=$(wget -O - "${git_ref_url}" | sed -ne 's/^.*"sha": "\(.*\)".*$/\1/p')
-    log "${LAST_GIT_COMMIT_ID}"
-    LAST_GIT_COMMIT=$(wget -O - "${git_commit_url}/$LAST_GIT_COMMIT_ID")
-    log "${LAST_GIT_COMMIT}"
+    printf '\nDownloading last version from git ...\n'
 
+    printf '\nDownloading last commit refrence from: %s\n' "${git_ref_url}"
+    LAST_COMMIT_ID=$(wget -O - "${git_ref_url}" | sed -ne 's/^.*"sha": "\(.*\)".*$/\1/p')
+
+    git_commit_url="${git_commit_url}/$LAST_COMMIT_ID"
+    printf 'Downloading last commit from: %s\n' "${git_commit_url}"
+    LAST_COMMIT=$(wget -O - "${git_commit_url}/$LAST_COMMIT_ID")
+
+    printf 'Downloading last version from: %s\n' "${git_contents_url}"
     wget -O "${FILENAME}" "${git_contents_url}"
   elif [[ $(type curl echo $? >/dev/null 2>&1) -eq 0 ]]; then
     printf 'Downloading last development version of Script from git:\n\n'
-    LAST_GIT_COMMIT_ID=$(curl "${git_ref_url}" | sed -ne 's/^.*"sha": "\(.*\)".*$/\1/p')
-    LAST_GIT_COMMIT=$(curl "${git_commit_url}/$LAST_GIT_COMMIT_ID")
+    LAST_COMMIT_ID=$(curl "${git_ref_url}" | sed -ne 's/^.*"sha": "\(.*\)".*$/\1/p')
+    LAST_COMMIT=$(curl "${git_commit_url}/$LAST_COMMIT_ID")
 
     curl -o "${FILENAME}" "${git_contents_url}"
   else
@@ -113,13 +114,14 @@ update() {
   fi
 
   # First date is Author, second date is Commit
-  COMMIT_DATE=$(echo "${LAST_GIT_COMMIT}" | sed -ne 's/^[[:space:]]*"date": "\(.*\)"[[:space:]]*$/\1/p' | tail -1)
-  log "${COMMIT_DATE}"
+  COMMIT_DATE=$(echo "${LAST_COMMIT}" | sed -ne 's/^[[:space:]]*"date": "\(.*\)"[[:space:]]*$/\1/p' | tail -1)
 
   # Set the retrieval date in just downloaded script.
-  sed -i -e "5,0 s/COMMIT_DATE='';/COMMIT_DATE='${COMMIT_DATE}';/" "${FILENAME}"
+  sed -i "s/COMMIT_DATE=.*/COMMIT_DATE='${COMMIT_DATE}'/" "${FILENAME}"
 
-  printf '\nThe development version of Script is saved as:\n"%s"\n\n' "${FILENAME}"
+  printf '\nThe last version of script was downloaded.\n\n'
+  mv "${FILENAME}" "${0}"
+  chmod +x "${0}"
   exit 0
 }
 
